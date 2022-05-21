62ba7a2a43c513bd927ad8ac742b53f94bd4a435
Andis Spriņķis <mail@andis.work>
2022-05-21 11:45:33 +0300

    revert

--- nix-script-util.bash
+++ nix-script-util.bash
@@ -1,10 +1,40 @@
 #!/usr/bin/env bash
-dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-. $dir/common
 
-# Functions that contain Bash specific syntax and features or depend on such functions.
+# Pad echo output with line breaks
 
-# Read concealed password input.
+echo_br_bottom() {
+  echo $1; echo
+}
+
+echo_br_top() {
+  echo; echo $1
+}
+
+echo_br_both() {
+  echo; echo $1; echo
+}
+
+# Annotate functions and stages of scripts
+
+fn_call() {
+  for fn in "$@"; do
+    echo_br_bottom "Function $fn has started..."
+    $fn
+    echo_br_bottom "Function $fn has completed!"
+  done
+}
+
+echo_stage_status() {
+  if [ "$1" -eq "0" ]; then
+    echo_br_bottom "Starting $2 stage..."
+  fi 
+
+  if [ "$1" -eq "1" ]; then
+    echo_br_bottom "$2 stage has completed!"
+  fi 
+}
+
+# Read concealed password input
 # Credit: Stéphane Chazelas - https://unix.stackexchange.com/a/223000
 
 read_pw_input() {
