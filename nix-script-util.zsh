0da75e63f0f4cbeea167c1e46db1167b853c0d06
Andis Spriņķis <mail@andis.work>
2022-05-21 11:39:31 +0300

    move bash and zsh compatible fns source to common script file

--- nix-script-util.zsh
+++ nix-script-util.zsh
@@ -1,35 +1,4 @@
 #!/usr/bin/env zsh
+. $dir/common
 
-# Pad echo output with line breaks
-
-echo_br_bottom() {
-  echo $1; echo
-}
-
-echo_br_top() {
-  echo; echo $1
-}
-
-echo_br_both() {
-  echo; echo $1; echo
-}
-
-# Annotate functions and stages of scripts
-
-fn_call() {
-  for fn in "$@"; do
-    echo_br_bottom "Function $fn has started..."
-    $fn
-    echo_br_bottom "Function $fn has completed!"
-  done
-}
-
-echo_stage_status() {
-  if [ "$1" -eq "0" ]; then
-    echo_br_bottom "Starting $2 stage...";
-  fi 
-
-  if [ "$1" -eq "1" ]; then; 
-    echo_br_bottom "$2 stage has completed!";
-  fi 
-}
+# Functions that contain ZSH specific syntax and features or depend on such functions.
